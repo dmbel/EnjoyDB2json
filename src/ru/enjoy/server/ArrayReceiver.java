f71a26267c9d6c775bfa94a15451b93ca9e0a6f8
dmbel <dm_bel@rambler.ru>
2016-05-21 18:08:56 +0400

    Готово

--- src/ru/enjoy/server/ArrayReceiver.java
+++
@@ -1,7 +0,0 @@
-package ru.enjoy.server;
-
-import ru.enjoy.server.exceptions.BadDataAnnotationException;
-
-public interface ArrayReceiver {
-	void putArray(String type, String[] vals) throws BadDataAnnotationException;
-}
